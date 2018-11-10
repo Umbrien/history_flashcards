97c331cab7000ca781678cd8d1791267665ee367
Umbrien <kalugin.serej@gmail.com>
2018-11-10 18:23:34 +0200

    Initial commit

---
+++ js/cards.js
@@ -0,0 +1,6 @@
+function showH1TitleDescription() {
+	document.getElementById("h1-title-description").style.display = "block";
+}
+function hideH1TitleDescription() {
+	document.getElementById("h1-title-description").style.display = "none";
+}
