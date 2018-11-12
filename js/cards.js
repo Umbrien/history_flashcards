68e0213e166d405567d500d328ae637789774185
Umbrien <kalugin.serej@gmail.com>
2018-11-12 13:58:00 +0200

    Moved class and functions to top

--- js/cards.js
+++ js/cards.js
@@ -8,22 +8,6 @@ function changeH1TitleDescriptionDisplay() {
 	}
 }
 
-function cardMaker(theme, number, date, text) {
-	this.theme = theme;
-	this.number = number;
-	this.date = date;
-	this.text = text
-}
-
-
-cardsArray = [
-new cardMaker("legends", 1, "18 грудня 1955", "Народився Стів Джобс"),
-new cardMaker("legends", 2, 1969, "Народився Лінус Торвальдс"),
-new cardMaker("legends", 3, 1984, "Народився Марк Цукерберг")
-]
-
-cardsArray = randArr(cardsArray);
-
 class Queue{
 	constructor() {
 		this.items = new Array()
@@ -52,6 +36,13 @@ class Queue{
 	}
 }
 
+function cardMaker(theme, number, date, text) {
+	this.theme = theme;
+	this.number = number;
+	this.date = date;
+	this.text = text
+}
+
 function randomInteger(min, max) {
     var rand = min - 0.5 + Math.random() * (max - min + 1)
     rand = Math.round(rand);
@@ -71,14 +62,6 @@ function randArr(list){
 	}
 	return l
 }
-
-cardsQueue = new Queue;
-
-cardsArray.forEach(function(item, i, cardsArray) {
-	cardsQueue.enqueue(item);
-});
-
-document.getElementById("h1-title").innerText = "Карток залишилось: " + cardsQueue.items.length;	
 
 function nextCard() {
 	document.getElementById("removeCardButton").removeAttribute("disabled");
@@ -104,3 +87,20 @@ function nextCard() {
 function removeCard() {
 	cardsQueue.delete_first()
 }
+
+
+cardsArray = [
+new cardMaker("legends", 1, "18 грудня 1955", "Народився Стів Джобс"),
+new cardMaker("legends", 2, 1969, "Народився Лінус Торвальдс"),
+new cardMaker("legends", 3, 1984, "Народився Марк Цукерберг")
+]
+
+cardsArray = randArr(cardsArray);
+
+cardsQueue = new Queue;
+
+cardsArray.forEach(function(item, i, cardsArray) {
+	cardsQueue.enqueue(item);
+});
+
+document.getElementById("h1-title").innerText = "Карток залишилось: " + cardsQueue.items.length;	
